cd5729bfeef2488b2eb7d7f19c91ef0147c8d529
leandroseverino <lseverino@gmail.com>
2016-05-25 09:21:02 -0300

    Adicionando nova versão do projeto como projeto Maven

---
+++ target/loja3-1.0-SNAPSHOT/lib/js/controllers/loginCtrl.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('loginCtrl', loginCtrl);
+
+    loginCtrl.$inject = ['$location', 'AuthenticationService', 'FlashService', '$rootScope'];
+    
+    function loginCtrl($location, AuthenticationService, FlashService, $rootScope) {
+        var vm = this;
+        vm.login = login;
+        var mensagemDeErro = "";
+
+        (function initController() {
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.ok) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password, response.obj);
+                    $location.path('/');
+                    msg(response);
+                } else {
+                	console.log("aaaaaa");
+                	vm.mensagemDeErro = response.msg;
+                	msgErro(response.msg);
+                    FlashService.Error(response.msg);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+        
+    }
+
+})();
